a8b666c3f41dbece663b45eff98c691c23db0d94
Kathrine Shaner <shaner.64@osu.edu>
2019-03-21 10:35:20 -0400

    3/21 updates

--- Front_End/project.js
+++ Front_End/project.js
@@ -2,7 +2,10 @@
 
 function onClick(){
 //Switches from a basic login prompt to Duo Auth
-
+    document.getElementByID();
+    //Get duo, hidden to visible
+    document.getElementsByClassName();
+    //Get login, visible to hidden
 }
 
 function registerListener(){
@@ -27,6 +30,33 @@ return (
         <p>We were unable to authenticate you at this time</p>
     </div>
 )
+}
+
+function mainContainer() {
+    return (
+       <div class="mainContainer">
+            This is where a login prompt will be
+            <div class="login">
+              {loginContainer}
+                <p>this will initially be hidden, will use DOM elements to toggle show and hidden
+                between this and the login prompt, and to trigger a switch b/n success/failure screens</p>
+           </div>
+            <div class="duo">
+         {duoAuthenticationContainer}
+           </div>
+             <div class="authenticated">
+              
+             </div>
+       </div>
+           )
+}
+
+function duoAuthenticationContainer() {
+    
+}
+
+function loginContainer() {
+    
 }
 
 //TODO: Figure out how to do the calls/responses, Register event handler, finish onClick Function
