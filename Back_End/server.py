bbc9f15c9ec3d4c801427c3ddc140163c517facd
Kathrine Shaner <shaner.64@osu.edu>
2019-03-20 22:23:53 -0400

    Tried to fix some of the errors in the server.py file

--- Back_End/server.py
+++ Back_End/server.py
@@ -1,18 +1,18 @@
-import http.server
-import socketserver
+import SocketServer as socketserver
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
 PORT = 8080
 # Handler = http.server.SimpleHTTPRequestHandler
 
-class Handler(): 
-	
 
+class Handler(BaseHTTPRequestHandler):
+    # with socketserver.TCPServer(("", PORT), Handler) as httpd:
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-	httpd.ikey = DITMW1Y90LZLQLB0DGMF
-	httpd.skey = ZurpCXglNKm31Hwyht8PFyqpGKaVa2egoQDKqKJK
-	httpd.akey = abcdefghijkllmnopqrstuvwxyzabcdefghijkllmnopqrstuvwxyz
-	httpd.host = api-7ab09f67.duosecurity.com
-    
+    httpd = socketserver.TCPServer(("", PORT), Handler)
+    httpd.ikey = "DITMW1Y90LZLQLB0DGMF"
+    httpd.skey = "ZurpCXglNKm31Hwyht8PFyqpGKaVa2egoQDKqKJK"
+    httpd.akey = "abcdefghijkllmnopqrstuvwxyzabcdefghijkllmnopqrstuvwxyz"
+    httpd.host = "api-7ab09f67.duosecurity.com"
+
     print("serving at port", PORT)
     httpd.serve_forever()
