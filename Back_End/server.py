8cf1a622b8654c1af5fd9098afa2353aa51ee1c6
Kathrine Shaner <shaner.64@osu.edu>
2019-03-24 16:45:34 -0400

    Got the server to work

--- Back_End/server.py
+++ Back_End/server.py
@@ -1,18 +1,20 @@
 import SocketServer as socketserver
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import SimpleHTTPServer
 
 PORT = 8080
-# Handler = http.server.SimpleHTTPRequestHandler
+Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
 
 
-class Handler(BaseHTTPRequestHandler):
+#class Handler():
     # with socketserver.TCPServer(("", PORT), Handler) as httpd:
 
-    httpd = socketserver.TCPServer(("", PORT), Handler)
-    httpd.ikey = "DITMW1Y90LZLQLB0DGMF"
-    httpd.skey = "ZurpCXglNKm31Hwyht8PFyqpGKaVa2egoQDKqKJK"
-    httpd.akey = "abcdefghijkllmnopqrstuvwxyzabcdefghijkllmnopqrstuvwxyz"
-    httpd.host = "api-7ab09f67.duosecurity.com"
+httpd = socketserver.TCPServer(("", PORT), Handler)
+httpd.ikey = "DITMW1Y90LZLQLB0DGMF"
+httpd.skey = "ZurpCXglNKm31Hwyht8PFyqpGKaVa2egoQDKqKJK"
+httpd.akey = "abcdefghijkllmnopqrstuvwxyzabcdefghijkllmnopqrstuvwxyz"
+httpd.host = "api-7ab09f67.duosecurity.com"
 
-    print("serving at port", PORT)
-    httpd.serve_forever()
+print("serving at port", PORT)
+print("Httpd ",httpd.host)
+httpd.serve_forever()
